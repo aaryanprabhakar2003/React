b8fc73e06a7a2e7c6517d8b2dda584fe2c2133e2
Aaryan Prabhakar <aaryanprabhakar07@gmail.com>
2022-05-23 10:45:25 +0530

    react 23 may

--- my-app/src/components/Component2.js
+++ my-app/src/components/Component2.js
@@ -10,12 +10,58 @@ export default function Component2() {
     }
     const handleOnChange=(event)=>{
         console.log("you attempted to change the text")
-        //setText(event.target.value);
+        setText(event.target.value);
     }
+    const replaceStr=()=>{
+      let repStr=text.replace("CU","TU");
+      setText(repStr);
+    
+    }
+    const handleOnClear=()=>{
+      let newText="";
+      setText(newText);
+
+
+    }
+    const submitvalidation=()=>{
+      //let newText="this is website";
+      //setText(newText);
+      if (text.length<1){
+        alert("please complete the form ")
+      }
+      else{
+        alert("form submitted")
+    }
+    handleOnClear()
+    }
+    const slice=()=>{
+      let newslice=text.slice(6,8);
+      setText(newslice)
+    }
+    const concat=()=>{
+       var text2="@gmail.com"
+      let newconcat=text.concat(text2);
+      setText(newconcat);
+    }
+
   return (
+    <>
     <div>
     <textarea className="form-control1" id="textbox" rows="10" cols="30" onChange={handleOnChange} value={text}></textarea><br></br>
-    <button class="btn btn-primary" onClick={handleOnClick}>Button</button>
+    <button class="btn btn-primary mx-2" onClick={handleOnClick}>Button</button>
+    <button class="btn btn-primary mx-2" onClick={replaceStr}>Replace</button>
+    <button class="btn btn-primary mx-2" onClick={handleOnClear}>Clear</button>
+    <button class="btn btn-primary mx-2" onClick={submitvalidation}>Submit</button>
+    <button class="btn btn-primary mx-2" onClick={slice}>Slice</button>
+    <button class="btn btn-primary mx-2" onClick={concat}>concat</button>
+
     </div>
-  )
+    <div classname='container-my-3'>
+    <h2>Summary</h2>
+    <p>Total number of words:{text.split(" ").length}.</p>
+    <p>Total number of characters:{text.length}.</p>
+    </div>
+  </>
+    )
+  
 }
