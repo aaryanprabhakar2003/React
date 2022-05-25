35605c7572e18caa9d6250f623dee6d23e725482
Aaryan Prabhakar <aaryanprabhakar07@gmail.com>
2022-05-25 12:10:21 +0530

    react 25 may

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,25 @@
+import React,{useState} from 'react'
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import Component2 from './components/Component2';
 function App() {
+  const[mode,setMode]=useState('light')
+  const toggleMode=()=>{
+    if(mode=='light'){
+      setMode("dark")
+    }
+    else{
+      setMode("light")
+    }
+  }
   return (
+
     <>
 <Navbar title=/*"Chitkara University Punjab Campus"*/
-"Chitkara University" abouttxt="About Cu"/>
+"Chitkara University" abouttxt="About Cu" mode={mode} toggleMode={toggleMode}/>
+
+
 <Component2/>
 </>
 
